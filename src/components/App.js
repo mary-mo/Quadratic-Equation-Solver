5a95a1f50d55534bd0616be9e6c847615bc5ee9a
Mary Melqonyan <boo.marymo@gmail.com>
2018-02-27 22:07:52 +0400

    Code improvments

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import Solution from './Solution';
 
-const isNumber = (value) => {
-  return value !== 0 && !isNaN(value);
-};
-
 class App extends Component {
   state = {
     a: '',
@@ -23,30 +19,30 @@ class App extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    const a = +this.state.a;
-    const b = +this.state.b;
-    const c = +this.state.c;
+
+    const a = this.state.a === '' ? 1 : +this.state.a;
+    const b = this.state.b === '' ? 1 : +this.state.b;
+    const c = this.state.c === '' ? 1 : +this.state.c;
 
     let x, x1, x2;
     const d = (Math.pow(b, 2) - 4 * a * c);
 
-    if (!(isNumber(a) && isNumber(b) && isNumber(c))) {
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
       this.setState({
         answer: 'Something went wrong!',
       });
-    } else {
-      if (d > 0) {
-        x1 = ((-b - Math.sqrt(d)) / (2 * a));
-        x2 = ((-b + Math.sqrt(d)) / (2 * a));
-        this.setState({ answer: `The equation has two roots! X1=${x1}, x2=${x2}`, })
-      }
-      if (d === 0) {
-        x = (-b / (2 * a));
-        this.setState({ answer: `Equation has one root! x=${x}`, })
-      }
-      if (d < 0) {
-        this.setState({ answer: 'Equation does not have a solution!', })
-      }
+    } else if (a === 0) {
+      x = (-c / b);
+      this.setState({ answer: `The equation has one root! X=${x}`, })
+    } else if (d > 0) {
+      x1 = ((-b - Math.sqrt(d)) / (2 * a));
+      x2 = ((-b + Math.sqrt(d)) / (2 * a));
+      this.setState({ answer: `The equation has two roots! X1=${x1}, x2=${x2}`, })
+    } else if (d === 0) {
+      x = (-b / (2 * a));
+      this.setState({ answer: `Equation has one root! x=${x}`, })
+    } else if (d < 0) {
+      this.setState({ answer: 'Equation does not have a solution!', })
     }
   };
 
